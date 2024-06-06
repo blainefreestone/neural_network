c9545c3fb1ac9c09c085593fff319db01334ce98
blainefreestone <blainefreestone01@gmail.com>
2024-06-05 22:23:52 -0600

    initializes based on input and output sizes

--- layer.py
+++ layer.py
@@ -1,8 +1,22 @@
+import numpy as np
+
 class Layer:
     """
     Class for internal layers of the network
     """
-    def __init__():
-        pass
+    def __init__(self, num_inputs: int, num_neurons: int) -> None:
+        """
+        Initialize the layer with the input and output sizes.
+
+        Args:
+            num_inputs: Number of input nodes
+            num_neurons: Number of output nodes
+        Returns:
+            None
+        """
+        np.random.seed(0)   # seed for reproducibility
+        self.weights = np.random.randn(num_inputs, num_neurons) # start layer with random weights
+        self.biases = np.zeros((1, num_neurons))    # start layer with zero biases
+
     def forward():
         pass
